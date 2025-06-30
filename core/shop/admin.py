bb1aebce7f6380f6d8db698643d3cb92deed2a1d
MATIN <mohammadmatin13872008@gmail.com>
2025-06-30 23:11:52 +0330

    Create Admin Shop

--- core/shop/admin.py
+++ core/shop/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import ProductView,ProductCategoryView,ProductImageView
+# ======================================================================================================================
+@admin.register(ProductView)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price','created_date', 'updated_date')  # نمایش برخی از فیلدهای محصول
+    search_fields = ('title', 'description')  # جستجو بر اساس نام و توضیحات
+    list_filter = ('category', 'created_date')  # فیلتر کردن محصولات بر اساس دسته‌بندی و تاریخ ایجاد
+    ordering = ('-created_date',)  # مرتب‌سازی محصولات بر اساس تاریخ ایجاد به صورت نزولی
+    prepopulated_fields = {'slug': ('title',)}  # پر کردن خودکار فیلد slug بر اساس نام
+    date_hierarchy = 'created_date'  # امکان فیلتر بر اساس تاریخ
 
-# Register your models here.
+# ======================================================================================================================
+@admin.register(ProductCategoryView)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title','created_date')  # نمایش نام، والد، وضعیت فعال بودن و تاریخ ایجاد
+    search_fields = ('title',)  # جستجو بر اساس نام دسته‌بندی
+    list_filter = ('created_date',)  # فیلتر بر اساس وضعیت فعال بودن و تاریخ ایجاد
+    prepopulated_fields = {'slug': ('title',)}  # پر کردن خودکار slug بر اساس نام
+
+# ======================================================================================================================
+@admin.register(ProductImageView)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product','created_date')  # نمایش محصول، تصویر، وضعیت برجسته بودن و تاریخ ایجاد
+    list_filter = ('created_date',)  # فیلتر بر اساس وضعیت برجسته بودن و تاریخ ایجاد
+
+# ======================================================================================================================
--- core/shop/views.py
+++ core/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView,DetailView
+from .models import ProductView,ProductStatusView
 # ======================================================================================================================
-class ProductsGridView(TemplateView):
+class ProductsGridView(ListView):
     template_name = "shop/product-grid.html"
+    queryset = ProductView.objects.filter(status=ProductStatusView.publish.value)
 # ======================================================================================================================
