b98bfbd3e52f7e989d8eddcf22daf533b97a7a9e
MATIN <mohammadmatin13872008@gmail.com>
2025-07-01 20:19:44 +0330

    Create Page Shop

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,8 +1,10 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
-from .models import ProductModels,ProductStatusModels
+from django.views.generic import ListView
+from .models import ProductModels, ProductStatusModels
 # ======================================================================================================================
 class ProductsGridView(ListView):
     template_name = "shop/product-grid.html"
     queryset = ProductModels.objects.filter(status=ProductStatusModels.publish.value)
+    paginate_by = 9
 # ======================================================================================================================
