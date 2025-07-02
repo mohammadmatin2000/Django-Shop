b6dfd41d498d3fca0024c3e33efbf28c90a961e1
MATIN <mohammadmatin13872008@gmail.com>
2025-07-02 19:25:27 +0330

    Create Filter Page

--- core/shop/views.py
+++ core/shop/views.py
@@ -6,6 +6,16 @@ from .models import ProductModels, ProductStatusModels, ProductCategoryModels
 class ProductsGridView(ListView):
     template_name = "shop/product-grid.html"
     paginate_by = 9
+
+    def get_paginate_by(self, queryset):
+        page_size = self.request.GET.get("page_size")
+        if page_size:
+            try:
+                return int(page_size)
+            except ValueError:
+                pass
+        return page_size
+
     def get_queryset(self):
         queryset = ProductModels.objects.filter(status=ProductStatusModels.publish.value)
         search = self.request.GET.get("q")
@@ -14,6 +24,9 @@ class ProductsGridView(ListView):
         category_id = self.request.GET.get("category_id")
         if category_id:
             queryset = queryset.filter(category__id=category_id)
+        order_by = self.request.GET.get("order_by")
+        if order_by:
+            queryset = queryset.order_by(order_by)
         return queryset
     def get_context_data(self, **kwargs):
         categories = ProductCategoryModels.objects.all()
