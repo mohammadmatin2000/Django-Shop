fd9315188f4756dcd23bd24c611ba2542e2f6145
MATIN <mohammadmatin13872008@gmail.com>
2025-07-03 14:01:07 +0330

    Create Similar_Products

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -1,14 +1,22 @@
 from django import template
+
 register = template.Library()
-from ..models import ProductModels,ProductStatusModels
+from ..models import ProductModels, ProductStatusModels
 # ======================================================================================================================
 @register.inclusion_tag('includes/latest-products.html')
 def latest_products():
     products = ProductModels.objects.filter(status=ProductStatusModels.publish.value).order_by('-created_date')[:8]
     return {'products': products}
 # ======================================================================================================================
+@register.inclusion_tag('includes/similar-products.html')
+def similar_products(product):
+    products_category = product.category.all()
+    products = ProductModels.objects.filter(status=ProductStatusModels.publish.value,
+                                            category__in=products_category).order_by('-created_date')[:4]
+    return {'products': products}
+# ======================================================================================================================
 @register.filter
-def formatted_price(value):
+def formatted_price():
     try:
         value = float(value)
         formatted = f"{value:,.3f}".replace(".", "٬").replace(".", "٫")
