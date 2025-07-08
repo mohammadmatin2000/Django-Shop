83e3e96215011bfa1af5be54e98d394c26909cb4
MATIN <mohammadmatin13872008@gmail.com>
2025-07-08 22:23:24 +0330

    Create Dashboard permission

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ..permission import HasAdminPermission
 # ======================================================================================================================
-class AdminDashboardHomeView(TemplateView):
+class AdminDashboardHomeView(LoginRequiredMixin,HasAdminPermission,TemplateView):
     template_name = "dashboard/admin/home.html"
 # ======================================================================================================================
--- core/dashboard/customer/views.py
+++ core/dashboard/customer/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ..permission import HasCustomerPermission
 # ======================================================================================================================
-class CustomerDashboardHomeView(TemplateView):
+class CustomerDashboardHomeView(LoginRequiredMixin,HasCustomerPermission,TemplateView):
     template_name = "dashboard/customer/home.html"
 # ======================================================================================================================
