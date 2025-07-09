5953180671ce4e52b2bf338f427ea90033baf3df
MATIN <mohammadmatin13872008@gmail.com>
2025-07-09 18:59:28 +0330

    Create ChangePassword

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import AdminDashboardHomeView
+from .views import AdminDashboardHomeView,AdminSecurityView
 # ======================================================================================================================
 app_name='admin'
 urlpatterns = [
     path('home/',AdminDashboardHomeView.as_view(),name='home'),
+    path('security/',AdminSecurityView.as_view(),name='security'),
 ]
 # ======================================================================================================================
--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,7 +1,17 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from ..permission import HasAdminPermission
+from .forms import PasswordChangeForm
 # ======================================================================================================================
 class AdminDashboardHomeView(LoginRequiredMixin,HasAdminPermission,TemplateView):
     template_name = "dashboard/admin/home.html"
+# ======================================================================================================================
+class AdminSecurityView(LoginRequiredMixin,HasAdminPermission,TemplateView,PasswordChangeView,SuccessMessageMixin):
+    template_name = "dashboard/admin/profile/security.html"
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy("dashboard:admin:security")
+    success_message = "رمز با موفقیت تغییر کرد"
 # ======================================================================================================================
