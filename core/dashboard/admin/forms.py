9ed4ca05269df70d3f8f83ed8ae2265fc2aaa7b6
MATIN <mohammadmatin13872008@gmail.com>
2025-07-09 21:19:51 +0330

    Create Message ChangePassword

--- core/dashboard/admin/forms.py
+++ core/dashboard/admin/forms.py
@@ -1,6 +1,12 @@
 from django.contrib.auth import forms as auth_forms
-
+from django.utils.translation import gettext_lazy as _
 # ======================================================================================================================
 class PasswordChangeForm(auth_forms.PasswordChangeForm):
-    pass
+    error_messages = {
+        **auth_forms.PasswordChangeForm.error_messages,
+        "password_incorrect": _(
+            "پسورد فعلی را اشتباه وارد کردید لطفا صحیح وارد کنید"
+        ),
+        "password_mismatch": _("پسورد های جدید با هم مطابقت ندارند"),
+    }
 # ======================================================================================================================
