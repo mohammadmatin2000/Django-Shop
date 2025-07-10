3e1ef0e0915302fa009596a8295d444cda57b782
MATIN <mohammadmatin13872008@gmail.com>
2025-07-10 19:31:18 +0330

    Create Dasheboard Image

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import AdminDashboardHomeView,AdminSecurityView,AdminProfileView
+from .views import AdminDashboardHomeView,AdminSecurityView,AdminProfileView,AdminProfileImageView
 # ======================================================================================================================
 app_name='admin'
 urlpatterns = [
     path('home/',AdminDashboardHomeView.as_view(),name='home'),
     path('security/',AdminSecurityView.as_view(),name='security'),
     path('profile/',AdminProfileView.as_view(),name='profile'),
+    path('profile/image/',AdminProfileImageView.as_view(),name='profile-image'),
 ]
 # ======================================================================================================================
--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.shortcuts import redirect
 from ..permission import HasAdminPermission
 from .forms import AdminPasswordChangeForm,AdminChangeProfileForm
 # ======================================================================================================================
@@ -22,4 +24,15 @@ class AdminProfileView(LoginRequiredMixin,HasAdminPermission,SuccessMessageMixin
     success_message = "اطلاعات با موفقیت تغییر کرد"
     def get_object(self, queryset=None):
         return self.request.user.user_profile
+# ======================================================================================================================
+class AdminProfileImageView(LoginRequiredMixin,HasAdminPermission,SuccessMessageMixin,UpdateView):
+    http_method_names = ["post"]
+    template_name = "dashboard/admin/profile/profile.html"
+    success_url = reverse_lazy("dashboard:admin:profile")
+    success_message = "عکس با موفقیت تغییر کرد"
+    def get_object(self, queryset=None):
+        return self.request.user.user_profile
+    def form_valid(self, form):
+        messages.error("آپلود عکس به مشکل خورده لطفا دوباره تلاش کنید")
+        return redirect(reverse_lazy(self.success_url))
 # ======================================================================================================================
