608f0a3fdd7dd64d056ae5575966301048afdf9d
MATIN <mohammadmatin13872008@gmail.com>
2025-07-16 20:02:21 +0330

    Create Template Checkout

--- core/order/views.py
+++ core/order/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .permission import HasCustomerPermission
 from django.views.generic import TemplateView
+from .models import UserAddressModel
 # ======================================================================================================================
 class OrderCheckOutView(HasCustomerPermission,LoginRequiredMixin,TemplateView):
     template_name = 'order/checkout.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['addresses'] = UserAddressModel.objects.filter(user=self.request.user)
+        return context
 # ======================================================================================================================
