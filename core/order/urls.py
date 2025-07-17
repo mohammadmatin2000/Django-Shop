5d8419ed623a4262cb47aa54a6b29a3f2125ce76
MATIN <mohammadmatin13872008@gmail.com>
2025-07-17 16:08:47 +0330

    Create Template Order-Complete

--- core/order/urls.py
+++ core/order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import OrderCheckOutView
+from .views import OrderCheckOutView,OrderCompleteView
 # ======================================================================================================================
 app_name = 'order'
 urlpatterns = [
     path('checkout/', OrderCheckOutView.as_view(), name='checkout'),
+    path('complete/', OrderCompleteView.as_view(), name='complete'),
 ]
 # ======================================================================================================================
--- core/order/views.py
+++ core/order/views.py
@@ -1,13 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .permission import HasCustomerPermission
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView, CreateView
 from .models import UserAddressModel
 from .forms import OrderCreateForm
 from cart.models import CartModels
+from django.urls import reverse_lazy
 # ======================================================================================================================
 class OrderCheckOutView(HasCustomerPermission,LoginRequiredMixin,FormView):
     template_name = 'order/checkout.html'
     form_class = OrderCreateForm
+    success_url = reverse_lazy('order:complete')
+
+    def form_invalid(self, form):
+        print(form.errors)
+        return super().form_invalid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -18,4 +24,8 @@ class OrderCheckOutView(HasCustomerPermission,LoginRequiredMixin,FormView):
         context["total_price"] = total_price
         context["total_tax"] = round((total_price * 9) / 100)
         return context
+# ======================================================================================================================
+class OrderCompleteView(HasCustomerPermission,LoginRequiredMixin,TemplateView):
+    template_name = 'order/complete.html'
+
 # ======================================================================================================================
